540951e5e6522216e1ea0367e68cf7547941d0b1
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-21 22:49:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]\w*\W\b/) != nil
+  if text.match(/^[A-Z].*\.$/) == nil
     false 
   else 
     true 

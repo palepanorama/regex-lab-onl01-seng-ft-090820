844886c8e691069ea00d9ece907ff1004234de1d
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-21 22:37:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,6 +3,7 @@ def starts_with_a_vowel?(word)
     true 
   else 
     false 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,7 +12,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.match(/^\w{5}\s/)
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)

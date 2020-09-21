dfa2bc26af4d387ce7e000ce94e348e126222b3d
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-21 22:40:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[AEIOU]| ^[aeiou]/)
-    true 
-  else 
-    false 
-  end 
+  if word.match(/\A[aeiouAEIOU]/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
